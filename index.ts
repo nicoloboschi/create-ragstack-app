14225f645d482d897f314ab87360f50257d4caf0
Nicolò Boschi <boschi1997@gmail.com>
2024-04-09 17:05:56 +0200

    fixes

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import prompts from 'prompts'
 import type { InitialReturnValue } from 'prompts'
 import checkForUpdate from 'update-check'
 import { updateApp } from './update-app'
-import { getPackageManager } from './helpers/package-manager'
+import { getPackageManager, PackageManager } from './helpers/package-manager'
 import packageJson from './package.json'
 import fs from 'fs'
 
@@ -35,12 +35,11 @@ const onPromptState = (state: {
 
 const command = new Commander.Command()
 command.name(packageJson.name)
+    .description(packageJson.description)
     .version(packageJson.version)
-    .arguments('<project-directory>')
-    .usage(`${green('<project-directory>')} [options]`)
-    .action((name) => {
-        projectPath = name
-    })
+command.command("install")
+    .alias("i")
+    .argument("[<version>]", "Ragstack version to install. If not provided, the latest version will be installed.")
     .option(
         '--use-npm',
         `
@@ -61,92 +60,68 @@ command.name(packageJson.name)
 
   Explicitly tell the CLI to bootstrap the application using Yarn
 `
-    );
+    )
+    .action(async (version: string, options: { useNpm: boolean, usePnpm: boolean, useYarn: boolean }) => {
+        const projectPath = process.cwd()
+        const packageJsonPath = path.join(projectPath, 'package.json')
+        if (!fs.existsSync(packageJsonPath)) {
+            console.log(
+                `\nPlease initialize the project at ${cyan(projectPath)} with your favourite package manager.`
+            )
+            process.exit(1)
+        }
+        const packageManager = !!options.useNpm ? 'npm' : !!options.useYarn ? 'yarn' : await getPackageManager(projectPath)
+        console.log(`Using ${cyan(packageManager)} as the package manager.`)
+        await updateApp({
+            appPath: projectPath,
+            packageManager,
+            ragstackVersion: version
+        })
+    });
 
 command.parse(process.argv)
 const program = command.opts()
 
-const packageManager = !!program.useNpm
-    ? 'npm'
-    : !!program.usePnpm
-        ? 'pnpm'
-        : !!program.useYarn
-            ? 'yarn'
-            : getPackageManager()
 
-async function run(): Promise<void> {
-    if (typeof projectPath === 'string') {
-        projectPath = projectPath.trim()
-    }
+// const update = checkForUpdate(packageJson).catch(() => null)
 
-    if (!projectPath) {
-        console.log(
-            '\nPlease specify the project directory:\n' +
-            `  ${cyan(program.name())} ${green('<project-directory>')}\n` +
-            'For example:\n' +
-            `  ${cyan(program.name())} ${green('my-ragstack-app')}\n\n` +
-            `Run ${cyan(`${program.name()} --help`)} to see all options.`
-        )
-        process.exit(1)
-    }
-    const packageJsonPath = path.join(projectPath, 'package.json')
-    if (!fs.existsSync(packageJsonPath)) {
-        console.log(
-            `\nPlease initialize the project at ${cyan(projectPath)} with your favourite package manager.`
-        )
-        process.exit(1)
-    }
+// async function notifyUpdate(): Promise<void> {
+//     try {
+//         const res = await update
+//         if (res?.latest) {
+//             const updateMessage =
+//                 packageManager === 'yarn' ? 'yarn global add create-ragstack-app' : 'npm i -g create-ragstack-app'
 
-    const resolvedProjectPath = path.resolve(projectPath)
-    await updateApp({
-        appPath: resolvedProjectPath,
-        packageManager
-    })
-}
+//             console.log(
+//                 yellow(bold('A new version of `create-ragstack-app` is available!')) +
+//                 '\n' +
+//                 'You can update by running: ' +
+//                 cyan(updateMessage) +
+//                 '\n'
+//             )
+//         }
+//         process.exit()
+//     } catch {
+//         // ignore error
+//     }
+// }
 
-const update = checkForUpdate(packageJson).catch(() => null)
+// run()
+//     // .then(notifyUpdate)
+//     .catch(async (reason) => {
+//         console.log()
+//         console.log('Aborting installation.')
+//         if (reason.command) {
+//             console.log(`  ${cyan(reason.command)} has failed.`)
+//         } else {
+//             console.log(
+//                 red('Unexpected error. Please report it as a bug:') + '\n',
+//                 reason
+//             )
+//         }
+//         console.log()
 
-async function notifyUpdate(): Promise<void> {
-    try {
-        const res = await update
-        if (res?.latest) {
-            const updateMessage =
-                packageManager === 'yarn'
-                    ? 'yarn global add create-ragstack-app'
-                    : packageManager === 'pnpm'
-                        ? 'pnpm add -g create-ragstack-app'
-                        : 'npm i -g create-ragstack-app'
+//         // await notifyUpdate()
 
-            console.log(
-                yellow(bold('A new version of `create-ragstack-app` is available!')) +
-                '\n' +
-                'You can update by running: ' +
-                cyan(updateMessage) +
-                '\n'
-            )
-        }
-        process.exit()
-    } catch {
-        // ignore error
-    }
-}
-
-run()
-    // .then(notifyUpdate)
-    .catch(async (reason) => {
-        console.log()
-        console.log('Aborting installation.')
-        if (reason.command) {
-            console.log(`  ${cyan(reason.command)} has failed.`)
-        } else {
-            console.log(
-                red('Unexpected error. Please report it as a bug:') + '\n',
-                reason
-            )
-        }
-        console.log()
-
-        // await notifyUpdate()
-
-        process.exit(1)
-    })
+//         process.exit(1)
+//     })
